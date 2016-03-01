95fd50e00b87b4276cd23d093d2284ee4faea075
Rauk <17raunak@gmail.com>
2016-03-01 19:41:09 +0530

    fd

--- src/main/java/com/raunak/practice/main/MainClass.java
+++ src/main/java/com/raunak/practice/main/MainClass.java
@@ -54,6 +54,16 @@ public class MainClass {
         iterable = db.getCollection("restaurants").find(Filters.eq("address.zipcode", "10075"));
         iterable = db.getCollection("restaurants").find(new Document("grades.grade", "B"));
         iterable = db.getCollection("restaurants").find(Filters.eq("grades.grade", "B"));
+        iterable = db.getCollection("restaurants").find(new Document("grades.score", new Document("$gt", 30)));
+        iterable = db.getCollection("restaurants").find(Filters.gt("grades.score", 30));
+        iterable = db.getCollection("restaurants").find(
+            new Document("cuisine", "Italian").append("address.zipcode", "10075"));
+        iterable = db.getCollection("restaurants").find(
+            new Document("$or", asList(new Document("cuisine", "Italian"), new Document("address.zipcode", "10075"))));
+        iterable = db.getCollection("restaurants").find(Filters.or(Filters.eq("cuisine", "Italian"),
+            Filters.eq("address.zipcode", "10075")));
+
+
         iterable.forEach(new Block<Document>() {
             //            @Override
             public void apply(final Document document) {
