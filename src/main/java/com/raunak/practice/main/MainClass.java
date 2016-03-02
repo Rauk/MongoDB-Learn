4a55e77bc715711bb6a53203f0ae4fb9256d543d
Rauk <17raunak@gmail.com>
2016-03-02 09:06:32 +0530

    only Sorting

--- src/main/java/com/raunak/practice/main/MainClass.java
+++ src/main/java/com/raunak/practice/main/MainClass.java
@@ -9,22 +9,27 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
-import  com.mongodb.client.model.Filters;
-//import com.mongodb.client.model.Filters.*;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Locale;
-
-import static java.util.Arrays.asList;
-
+import com.mongodb.client.model.Sorts;
 import java.text.ParseException;
-
 
 public class MainClass {
     public static void main(String[] args) throws ParseException {
         MongoClient mongoClient = new MongoClient();
         MongoDatabase db = mongoClient.getDatabase("test");
-        
+
+        FindIterable<Document> iterable = db.getCollection("restaurants").find()
+            .sort(new Document("borough", 1).append("address.zipcode", 1));
+        iterable.forEach(new Block<Document>() {
+            public void apply(final Document document) {
+                System.out.println(document);
+            }
+        });
+        System.out.println("\n\n\n");
+        iterable = db.getCollection("restaurants").find().sort(Sorts.ascending("borough", "address.zipcode"));
+        iterable.forEach(new Block<Document>() {
+            public void apply(final Document document) {
+                System.out.println(document);
+            }
+        });
     }
 }
